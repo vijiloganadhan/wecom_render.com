42331595586609bd1b225b0a727b8c1bd2ba93fc
vijiloganadhan <mlvijayalakshmiloganadhan@gmail.com>
2025-08-28 08:24:51 +0530

    new

--- flipcart_ecom/flipcart_ecom/wsgi.py
+++ flipcart_ecom/flipcart_ecom/wsgi.py
@@ -1,3 +1,10 @@
 import os; 
 from django.core.wsgi import get_wsgi_application; 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flipcart_ecom.settings');application = get_wsgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flipcart_ecom.settings');
+import django
+from django.core.management import call_command
+
+django.setup()
+call_command("migrate", interactive=False)
+
+application = get_wsgi_application()

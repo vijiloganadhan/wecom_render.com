ba9b1928ec8966c07a4900129e74f9d64ae8ecf8
vijiloganadhan <mlvijayalakshmiloganadhan@gmail.com>
2025-08-28 19:52:54 +0530

    new 1

--- flipcart_ecom/flipcart_ecom/settings.py
+++ flipcart_ecom/flipcart_ecom/settings.py
@@ -95,13 +95,13 @@ DATABASES = {
 #     )
 # }
 import dj_database_url
+
 DATABASES = {
     'default': dj_database_url.config(
         default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

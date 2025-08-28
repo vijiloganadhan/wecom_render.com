d86a8a24b31181eecf46d0d5ddc13df69660cfdd
vijiloganadhan <mlvijayalakshmiloganadhan@gmail.com>
2025-08-28 07:22:17 +0530

    new update for database

--- flipcart_ecom/flipcart_ecom/settings.py
+++ flipcart_ecom/flipcart_ecom/settings.py
@@ -87,14 +87,16 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-import dj_database_url
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://wecart_ecom_image_user:JSeICXxfCN6GfHGnIezCKv3XEG69ghnI@dpg-d2msif8gjchc73d2mhdg-a/wecart_ecom_image',
-        conn_max_age=1000
-    )
-}
-
+# import dj_database_url
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='postgresql://wecart_ecom_image_user:JSeICXxfCN6GfHGnIezCKv3XEG69ghnI@dpg-d2msif8gjchc73d2mhdg-a/wecart_ecom_image',
+#         conn_max_age=1000
+#     )
+# }
+import  dj_database_url
+database_url=os.environ.get("DATABASE_URL")
+DATABASES['default']=dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
